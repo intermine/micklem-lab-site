d8c12ca7ccf46b7700ce71c8dca5453be783a902
Radek Stepan <dev@radekstepan.com>
2014-01-16 10:44:22 +0000

    dokku enabled

--- export.js
+++ export.js
@@ -4,6 +4,6 @@ var service = require('blad');
 
 var config  = require('./config.json');
 
-config.mongodb = process.env.DATABASE_URL || config.mongodb;
+config.mongodb = process.env.MONGO_URL || process.env.DATABASE_URL || config.mongodb;
 
 service.db.export(config, __dirname, process.exit);
